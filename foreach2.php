2bc7e25670599a2f59a29a41b099fd18bd91a93a
Mars <marianomarquis@gmail.com>
2015-07-14 14:50:56 -0500

    For each 2

---
+++ foreach2.php
@@ -0,0 +1,30 @@
+<?php
+  $value = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+  foreach ($value as $values) {
+    if (is_integer($values)) {
+      echo "{$values} is an Int\n" . PHP_EOL;
+    }else if (is_float($values)) {
+      echo "{$values} is a float\n" . PHP_EOL;
+    }else if(is_bool($values)) {
+      echo "{$values} is a bool\n" . PHP_EOL;
+    }else if (is_array($values)) {
+      echo "{$values} is an array\n" . PHP_EOL;
+    }else if (is_null($values)) {
+      echo "{$values} is null\n" , PHP_EOL;
+    }else if (is_string($values)) {
+      echo "{$values} is a string\n" . PHP_EOL ;
+    }
+  }
+  
+  $Stuff = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", FALSE, (string) 11);
+  foreach ($Stuff as $ffutS) {
+  	if (is_array($ffutS)) {
+  		echo "Array ";
+  		foreach ($ffutS as $value) {
+  			echo "$value ";
+  		}
+  		echo "\n";
+  	} else {
+  		echo "$ffutS\n";
+  	}
+  }
