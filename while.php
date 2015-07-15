7e423ac4310eb46edb322eeeb39a704a121796a2
Mars <marianomarquis@gmail.com>
2015-07-15 08:41:54 -0500

    Updation

--- while.php
+++ while.php
@@ -1,7 +1,8 @@
 <?php
   $test = 5;
   while ($test <= 15) {
-    echo $test . PHP_EOL;
+    if ($test % 4 == 0 && $test % 2 == 0)
+    echo "$test is divisible by 4 and 2" . PHP_EOL;
     $test++;
   }
  ?>
