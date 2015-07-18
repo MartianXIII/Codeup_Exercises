1d05f114fc4d6e3d5bf9506d115c79c6344ca0b9
Mars <marianomarquis@gmail.com>
2015-07-18 18:42:16 -0500

    What is this i dont even

--- arithmetic.php
+++ arithmetic.php
@@ -1,23 +1,42 @@
 <?php
 //Mars Martian
 function add($a, $b) {
+  $a = 11;
+  $b = g;
+    if (is_numeric($a) && is_numeric($b)) {
     return ($a + $b) . "\n";
+} else {
+      return "ERROR: {$a} && {$b} arguments must be numbers\n";
+  }
 }
-
 function subtract($a, $b) {
+    if (is_numeric($a) && is_numeric($b))
     return ($a - $b) . "\n";
 }
 
 function multiply($a, $b) {
+    if (is_numeric($a) && is_numeric($b))
     return ($a * $b) . "\n";
 }
 
 function divide($a, $b) {
+    if (is_numeric($a) && is_numeric($b))
     return ($a / $b) . "\n";
 }
 
 function modulus($a, $b) {
+    if (is_numeric($a) && is_numeric($b))
     return ($a % $b) . "\n";
+}
+
+// uses print_r or var_dump on an array
+function inspect($variable = null, $dump = true) {
+    // Same as if ($dump == true) {...}
+    if ($dump) {
+        var_dump($variable);
+    } else {
+        print_r($variable);
+    }
 }
 
 $c = 10;
