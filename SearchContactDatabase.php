a06bdb72b61a41dd350a9dc9abbbf3348d751037
Mars <marianomarquis@gmail.com>
2015-08-01 12:00:44 -0500

    alpha v .01

---
+++ SearchContactDatabase.php
@@ -0,0 +1,17 @@
+<?php  ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Search Contacts</title>
+  </head>
+  <body>
+    <h3>Search the Contact Database</h3>
+    <p>You may seach by first or last name</p>
+    <form method="post" action="search.php?go" id="searchform">
+      <input type="text" name="name">
+      <input type="submit" name="submit" value="Search">
+    </form>
+    <<?php // HACK:  ?>
+  </body>
+</html>
