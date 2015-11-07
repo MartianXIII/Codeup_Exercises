aeb2a758ac39470bafb5b79ce826d2c554580e51
Mars <marianomarquis@gmail.com>
2015-11-07 11:34:34 -0600

    refactored

--- search-arrays.php
+++ search-arrays.php
@@ -1,34 +1,28 @@
 <?php
-$names = ['Tina', 'Dana', 'Mike', "Amy", 'Adam'];
+// Function returning Truthness if Value is found
+//  Search Tina and Bob in $names, is it works as expected.
+$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
-$result = array_search('Dana', $names);
-//Create a function that returns TRUE or FALSE
-//if an array value is found. Search for Tina and Bob
-//in $names. Make sure it works as expected.
-function isInArray($array, $value) {
-	return in_array($value, $array);
+function searchThis($query, $array){
+    $result = array_search($query, $array);
+    if ($result !== false) {
+        return true;
+    } else {
+        return false;
+    }
 }
-
-function compare($array1, $array2) {
-	$count = 0;
-	foreach ($array1 as $key => $value) {
-		if(is_numeric(array_search($value, $array2))) {
-		$count++;
-		}
-	}
-		return $count;
-
-//var_dump($count);
-//var_dump($compare);
+var_dump(searchThis('Tina', $names)) . PHP_EOL;
+var_dump(searchThis('Bob', $names)) . PHP_EOL;
+// Create a function to compare 2 arrays that returns the number of values
+// in common between the arrays. Use the 2 example arrays and
+// make sure your solution uses array_search().
+function compareThis($ar1, $ar2){
+    foreach ($ar1 as $key => $value) {
+        $result = array_search($value, $ar2);
+        if ($result !== false) {
+            $commonVariables += 1;
+        }
+    }
+    return $commonVariables;
 }
-
-if ($result) {
-	echo $names[$result];
-	var_dump($result);
-}
-
-var_dump(isInArray($names, 'Tina'));
-var_dump(isInArray($names, 'Bob'));
-var_dump(compare($names, $compare)) . PHP_EOL;
-echo (compare($names, $compare)) . PHP_EOL;
-//var_dump($array1 . $array2);
+var_dump(compareThis($names, $compare));
